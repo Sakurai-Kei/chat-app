8f16c73ad252c71554e722e9e393ac68d0dae57f
R_Ashfell <luqmanulraiserhakim@gmail.com>
2022-02-24 12:07:17 +0800

    basic ui done. moving on to integrate app with firebase

--- src/ChatRoom.tsx
+++ src/ChatRoom.tsx
@@ -1,8 +1,20 @@
+
 function ChatRoom() {
     return(
         <div className="chatRoom chatRoom-container">
-            <div>Child Element 1</div>
-            <div>Child Element 2</div>
+            <div className="chatRoom-header">
+                <div className="icon-bar">
+                    <span className="material-icons md-24">menu</span>
+                </div>
+            </div>
+            <div className="chatRoom-chatbox">
+                <div className="other-user">someone's chat</div>
+                <div className="current-user">my chat</div>
+            </div>
+            <div className="chatRoom-type">
+                <input className="messages" type="text" id="messages" name="messages"></input>
+                <span className="material-icons chatRoom-center">keyboard_return</span>
+            </div>
         </div>
     );
 }
