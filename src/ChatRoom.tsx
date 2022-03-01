bd276072eefeb24f7e69e85e9a70b96d65403107
R_Ashfell <luqmanulraiserhakim@gmail.com>
2022-03-01 15:40:55 +0800

    Auth is now owrking and firestore data retrieval is half-way done

--- src/ChatRoom.tsx
+++ src/ChatRoom.tsx
@@ -1,5 +1,35 @@
+import { time } from "console";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, onSnapshot, doc, query } from "firebase/firestore";
+import firebaseConfig from "./firebaseConfig";
+
+async function getMessages() {
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore();
+
+    const messages = query(collection(db, "messages"));
+    const subToMessages = onSnapshot(messages, (querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+        const { message, timestamp, userID } = doc.data();
+        console.log(message,timestamp,userID);
+        })
+    })
+   
+    // getMessages.forEach((doc) => {
+    //     console.log(doc.id, "=>", doc.data());
+    //     const {message, timestamp, userID } = doc.data();
+    //     console.log(message);
+    //     console.log(userID);
+    //     console.log(timestamp);
+    // })
+
+    // return getMessages;
+}
+
 
 function ChatRoom() {
+    getMessages();
+
     return(
         <div className="chatRoom chatRoom-container">
             <div className="chatRoom-header">
@@ -8,6 +38,7 @@ function ChatRoom() {
                 </div>
             </div>
             <div className="chatRoom-chatbox">
+                {}
                 <div className="other-user">someone's chat</div>
                 <div className="current-user">my chat</div>
             </div>
--- src/LogIn.tsx
+++ src/LogIn.tsx
@@ -1,17 +1,41 @@
 import { useNavigate } from "react-router-dom";
-
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import firebaseConfig from './firebaseConfig';
 
 function LogIn() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleClick = (event: unknown) => {
-        navigate("/chat-room")
+  const app = initializeApp(firebaseConfig);
+
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+  auth.languageCode = "it";
+
+  async function googleSignIn() {
+    try {
+    let result = await signInWithPopup(auth, provider);
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential?.accessToken;
+    const user = result.user;
+    if(user) {
+      navigate("/chat-room")
     }
+
+    }
+    catch(error: any) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.email;
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      console.error(error);
+    }
+  }
 
     return(
         <div className="login login-container">
             <div>Please choose your login method</div>
-            <button type="button" onClick={handleClick}  className="btn btn-login-google">Login with Google Account</button>
+            <button type="button" onClick={googleSignIn}  className="btn btn-login-google">Login with Google Account</button>
         </div>
     );
 }
